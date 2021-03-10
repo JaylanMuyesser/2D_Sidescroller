ec36c1ae558daf5ddd07128aaf9c189d0e4cfaf6
Luc <luc980@y7mail.com>
2021-03-10 18:07:14 +1100

    Starting enemyJump

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -2,31 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyScript : MonoBehaviour {
+public class EnemyScript : MonoBehaviour
+{
     [SerializeField] private float speedMultiplier = 25f;
     [SerializeField] private Transform enemy, respawnPoint;
     public GameObject Player;
     private PlayerScript healthCheck, invincibilityCheck;
     private Collider2D enemyCollider;
 
-    void Start() {
+    //[SerializeField]
+    //private float enemyJump; // Variable float declared for enemyJumpHeight
+    [SerializeField]
+    private float enemyJumpSpeed = 10; // variable float declared for enemyJumpSpeed
+    /*
+    [SerializeField]
+    private bool enemyGrounded;
+    
+    //private float enemyJumpTimer = 10;
+
+    private Rigidbody2D enemyRb; // a Rigidbody2D being declared  for a variable
+    */
+
+
+    void Start()
+    {
         healthCheck = Player.GetComponent<PlayerScript>();
         invincibilityCheck = Player.GetComponent<PlayerScript>();
         enemyCollider = GetComponent<Collider2D>();
+
+        enemyRb = enemyRb.GetComponent<Rigidbody2D>(); // making sure the variable gets the RigidBody2D component on start
+
+       // StartCoroutine(MakeEnemyJump);
     }
 
-    void Update() {
+    void Update()
+    {
         transform.Translate(Vector2.left * Time.deltaTime * speedMultiplier);
         speedMultiplier += Time.deltaTime * 0.20f;
 
-        if (healthCheck.playerHealth == 0) {
+
+
+
+        if (healthCheck.playerHealth == 0)
+        {
             Destroy(gameObject);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.collider.gameObject.CompareTag("Player")) {
-            if (invincibilityCheck.invincibilityLength <= 0) {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.CompareTag("Player"))
+        {
+            if (invincibilityCheck.invincibilityLength <= 0)
+            {
                 enemy.transform.position = respawnPoint.transform.position;
             }
             else
@@ -35,4 +63,17 @@ public class EnemyScript : MonoBehaviour {
             }
         }
     }
+/*    
+    private void FixedUpdate()
+    {
+        
+        enemyRb.AddForce(Vector2.up * enemyJumpSpeed, ForceMode2D.Impulse); // on frame it sets the velocity multiplied by enemyJumpSpeed
+    }
+    
+    private IEnumerator MakeEnemyJump(0)
+    {
+        yield return new WaitForSeconds(enemyJumpTimer);
+        enemyRb.AddForce(Vector2.up * enemyJumpSpeed, ForceMode2D.Impulse); // on frame it sets the velocity multiplied by enemyJumpSpeed
+    }
+ */
 }
