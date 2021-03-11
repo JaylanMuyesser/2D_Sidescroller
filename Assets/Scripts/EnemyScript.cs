4c0e17a36c5f0eea8e01565d7f47cf9a52829c8c
Luc <luc980@y7mail.com>
2021-03-11 17:59:33 +1100

    Added EnemyJump(?)

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -7,12 +7,22 @@ public class EnemyScript : MonoBehaviour {
     [SerializeField] private Transform enemy, respawnPoint;
     public GameObject Player;
     private PlayerScript healthCheck, invincibilityCheck;
+    
+    
     private Collider2D enemyCollider;
+    private bool isEnemyGrounded = true;
+    private Rigidbody2D enemyRB;
+    [SerializeField]
+    private float enemyJumpSpeed = 0.1f;
 
+ 
     void Start() {
         healthCheck = Player.GetComponent<PlayerScript>();
         invincibilityCheck = Player.GetComponent<PlayerScript>();
         enemyCollider = GetComponent<Collider2D>();
+        enemyRB = GetComponent<Rigidbody2D>();
+
+        StartCoroutine(enemyJumping());
     }
 
     void Update() {
@@ -22,6 +32,8 @@ public class EnemyScript : MonoBehaviour {
         if (healthCheck.playerHealth == 0) {
             Destroy(gameObject);
         }
+
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
@@ -34,5 +46,59 @@ public class EnemyScript : MonoBehaviour {
                 enemy.transform.position = respawnPoint.transform.position;
             }
         }
+
+        //if(collision.comparetag(ground))
+        //set isEnemyGrounded to true
+
+        if (collision.collider.gameObject.CompareTag("Ground"))
+        {
+            isEnemyGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        //if(collision.comparetag(ground))
+        //set isEnemyGrounded to false
+        if (collision.collider.gameObject.CompareTag("Ground"))
+        {
+            isEnemyGrounded = false;
+        }
+        
+    }
+
+
+    /// <summary>
+    /// This function will make enemy jump if grounded
+    /// </summary>
+    private IEnumerator enemyJumping()
+    {
+        while (true)
+        {
+            
+
+            if (isEnemyGrounded)
+            {
+                enemyRB.AddForce(Vector2.up * enemyJumpSpeed, ForceMode2D.Impulse); // on frame it sets the velocity multiplied by enemyJumpSpeed
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
+
+
+
+/* Luc's notes ; making an enemy jump
+First need variables to declare how high the enemy is jumping(done?)
+Need a rigid body and a collider for enemy (done)
+Need to make sure to check if enemy is grounded (done)
+Make the enemy jump (done)
+need to add force for rigid to make jump (done)
+Need to make sure to  check if enemy is in the air. (yeah)
+Need to make sure to enemy lands(yeah)
+then set a timer (done)
+
+
+  
+  
+*/
